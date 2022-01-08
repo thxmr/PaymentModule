4e3f06ea32ed55615b7b55c7f879ac446ce7d48b
Florian Verheecke <florian.verheecke.pro@gmail.com>
2022-01-08 18:54:50 +0100

    Adding new request to the api
    added : GET:/invoice and factures.php

--- backend/index.js
+++ backend/index.js
@@ -47,6 +47,52 @@ app.get('/invoices/:client_id', async function (req, res) {
     }
 });
 
+app.get('/invoice/:id', async function (req, res) {
+    // Initialization
+    let data = {};
+    let SQL = "";
+
+    // Preparing SQL request
+    SQL = `SELECT * FROM invoices WHERE transaction_id = ${req.params.id}`
+
+    // Getting SQL datas
+    let pgRes = await db.query(SQL);
+
+    // If fetch data
+    if(pgRes.rows.length > 0) {
+        // Prepare SQL request to get the subscription details
+        SQL = `SELECT * FROM subscriptions WHERE subscription_type = ${pgRes.rows[0].subscription_type}`;
+        // Send the SQL request
+        let subTypeRes = await db.query(SQL);
+
+        // If fetch sub details
+        if(subTypeRes.rows.length > 0) {
+
+            // Prepare invoice details
+            data = {
+                "transaction_id" : req.params.id,
+                "sub_name" : subTypeRes.rows[0].subname,
+                "price" : subTypeRes.rows[0].price,
+                "client_id" : pgRes.rows[0].client_id,
+                "name" : pgRes.rows[0].name,
+                "address" : pgRes.rows[0].address,
+                "payment_method" : pgRes.rows[0].payment_method
+            }
+
+            // Return details of the invoice
+            return res.status(200).send(data)
+        }
+        else {
+            // Return internal error
+            return res.status(500).send({'error' : "Internal server error"});
+        }
+    }
+    else {
+        // Return error
+        return res.status(404).send({'error' : "Wrong transaction id"})
+    }
+});
+
 app.post('/invoice', async function (req, res) {
     // Initialization
     let data = [];
@@ -57,11 +103,11 @@ app.post('/invoice', async function (req, res) {
     data['payment_method'] = req.body.payment_method;
     data['client_id'] = req.body.client_id
 
-    
+
 
     // Prepare the request
     let SQL = `
-    INSERT INTO invoices (subscription_type, client_id, address, payment_method, date) 
+    INSERT INTO invoices (subscription_type, client_id, address, payment_method, date)
     VALUES ('${data['subType']}', '${data['client_id']}', '${data['address']}', '${data['payment_method']}', '${new Date().toISOString().slice(0, 10)}')
     RETURNING transaction_id;
     `
@@ -81,7 +127,7 @@ app.post('/invoice', async function (req, res) {
         return res.status(500).send({'error' : pgRes})
     }
 });
-   
+
 app.get('/subscription/:id', async function (req, res) {
     // Initialization
     let SQL = ""
