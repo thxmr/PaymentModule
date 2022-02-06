c97fdf55d7b689c0e940850360c6e897c4a749c7
thxmr <58337512+thxmr@users.noreply.github.com>
2022-02-06 21:22:35 +0100

    Integration of all the invoices informations

--- frontend/Payment Form/factures.php
+++ frontend/Payment Form/factures.php
@@ -15,7 +15,6 @@ $clientClient = new Client([
 
 $res = $clientPayment->request('GET', 'invoice/' . $_GET['transaction_id']);
 $body = get_object_vars(json_decode($res->getBody()));
-print_r($body[transaction_id);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -34,19 +33,29 @@ print_r($body[transaction_id);
                     <img src="img/validated.jpg" alt="valide" width=100 height=100/>
                     <p>Achat Terminé</p>
                     <hr/>
-                    <p>Abonnement n°XXXXXX - Informations à rajouter - 29.99€</p>
+                    <p>Abonnement n°<?php print_r($body[transaction_id]);?>- <?php print_r($body[price]);?></p>
                 </div>
             </div>
             <div class="row" id="detailsrow">
                 <div class="col" id="detailclient">
-                    <p>#idclient Nom Prénom</p>
+                    <p><?php print_r($body[client_id]);?> <?php print_r($body[sub_name]);?></p>
                     <hr/>
-                    <p>Adresse de facturation</p>
+                    <p><?php print_r($body[address]);?></p>
                 </div>
                 <div class="col" id="detailscol">
                     <p>Commande #"insérer n° commande"</p>
                     <hr/>
-                    <p>"Insérer méthode de paiement"</p>
+                    <p><?php switch ($body[transaction_id]){
+                                case 1:
+                                    echo "Carte Bleue";
+                                case 2 :
+                                    echo "Paypal";
+                                case 3 :
+                                    echo "Apple Pay";
+                                default:
+                                    echo "Méthode de paiement inconnue";
+                            }
+                        ?></p>
                     <hr/>
                     <p>"Insérer date et heure du paiement</p>
                 </div>
